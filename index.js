fab817877193c18eaccf47494524d9f374d6f453
Avaer <a@kazmer.io>
2016-04-03 00:53:40 -0700

    Precompute initial octaves

--- index.js
+++ index.js
@@ -3,113 +3,52 @@
 
 const Alea = require('alea');
 const FastSimplexNoise = require('fast-simplex-noise');
+const Histogram = require('./src/histogram');
 
-const floor = Math.floor;
+const constants = require('./src/constants');
+const MIN_PRECOMPUTED_OCTAVES = constants.MIN_PRECOMPUTED_OCTAVES;
+const MAX_PRECOMPUTED_OCTAVES = constants.MAX_PRECOMPUTED_OCTAVES;
 
-const BUCKETS = 100;
-const SAMPLE_SCALE = 1e6;
-const SAMPLE_SIZE = 1e5;
+const PRECOMPUTED_HISTOGRAMS = require('./precomputed/histograms.json');
 
-function Histogram(opts) {
-  opts = opts || {};
-  const size = opts.buckets || BUCKETS;
-  const min = opts.min || 0;
-  const max = opts.max || 1;
-
-  this._buckets = (() => {
-    const buckets = Array(size);
-    for (let i = 0, l = buckets.length; i < l; i++) {
-      buckets[i] = 0;
-    }
-    return buckets;
-  })();
-  this._size = 0;
-  this._min = min;
-  this._max = max;
-}
-Histogram.prototype = {
-  getBucket: function(v) {
-    return ((v - this._min) / (this._max - this._min)) * this._buckets.length;
-  },
-  getBucketIndex: function(v) {
-    return floor(this.getBucket(v));
-  },
-  getBucketResidual: function(v) {
-    return this.getBucket(v) % 1;
-  },
-  add: function(v) {
-    const bucketIndex = this.getBucketIndex(v);
-    this._buckets[bucketIndex]++;
-    this._size++;
-  },
-  size: function() {
-    return this._size;
-  },
-  total: function() {
-    let result = 0;
-    for (let i = 0, l = this._buckets.length; i < l; i++) {
-      const bucketValue = this._buckets[i];
-      result += bucketValue;
-    }
-    return result;
-  },
-  normalizedBuckets: function() {
-    const total = this.total();
-    const normalizedBuckets = this._buckets.map(bucketValue => bucketValue / total);
-    return normalizedBuckets;
-  },
-  normalizedBucketValue: function() {
-    return 1 / this._buckets.length;
-  },
-  makeScaler: function() {
-    const normalizedBuckets = this.normalizedBuckets();
-    const cumulativeNormalizedBuckets = (() => {
-      let acc = 0;
-      return normalizedBuckets.map(bucketValue => {
-        const result = acc;
-        acc += bucketValue;
-        return result;
-      });
-    })();
-    const cdf = v => {
-      const bucketIndex = this.getBucketIndex(v);
-      const left = cumulativeNormalizedBuckets[bucketIndex];
-      const right = (bucketIndex < (cumulativeNormalizedBuckets.length - 1)) ? cumulativeNormalizedBuckets[bucketIndex + 1] : 1;
-      const bucketResidual = this.getBucketResidual(v);
-      return left + (bucketResidual * (right - left));
-    };
-    return cdf;
+function _getScaler(octaves) {
+  if (octaves >= MIN_PRECOMPUTED_OCTAVES && octaves <= MAX_PRECOMPUTED_OCTAVES) {
+    const histogramJson = PRECOMPUTED_HISTOGRAMS[octaves];
+    const histogram = Histogram.load(histogramJson);
+    const scaler = histogram.makeScaler();
+    return scaler;
+  } else {
+    throw new Error('octaves must be in (' + MIN_PRECOMPUTED_OCTAVES + ',' + MAX_PRECOMPUTED_OCTAVES + ')');
   }
-};
-
-function makeNoiseScaler(noise, sampler, predicate, opts) {
-  const histogram = new Histogram(opts);
-  while (!predicate(histogram)) {
-    const x = sampler();
-    const y = sampler();
-    const v = noise.in2D(x, y);
-    histogram.add(v);
-  }
-  return histogram.makeScaler();
 }
 
 function FastUniformNoise(opts) {
   opts = opts || {};
   opts.min = opts.min || 0;
   opts.max = opts.max || 1;
+  opts.frequency = opts.frequency || 1;
+  opts.octaves = opts.octaves || 1;
   opts.random = opts.random || new Alea('');
 
-  this._noise = new FastSimplexNoise(opts);
+  // console.log('uniform noise', opts);
 
-  const sampler = () => opts.random() * SAMPLE_SCALE;
-  const predicate = histogram => histogram.size() >= SAMPLE_SIZE;
-  this._scaler = makeNoiseScaler(this._noise, sampler, predicate, opts);
+  this._min = opts.min;
+  this._max = opts.max;
+  this._noise = new FastSimplexNoise({
+    min: 0,
+    max: 1,
+    frequency: opts.frequency,
+    octaves: opts.octaves,
+    random: opts.random,
+  });
+  this._scaler = _getScaler(opts.octaves);
 }
 FastUniformNoise.prototype = {
   in2D: function(x, y) {
     const v = this._noise.in2D(x, y);
     const scaledV = this._scaler(v);
-    return scaledV;
+    const offsetV = this._min + (scaledV * (this._max - this._min));
+    return offsetV;
   }
 };
 
